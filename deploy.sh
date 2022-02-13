163374d74a88eb4b20370a52403cb0a1e7074947
yifeewang <896678534@qq.com>
2022-02-13 15:13:17 +0800

    feat(wyf-design): deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'feat(wyf-design): deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:yifeewang/wyf-design.github.io.git master
+git push -f git@github.com:yifeewang/wyf-design.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
